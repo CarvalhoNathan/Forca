d494528f69bdc72f21b330c2ff628af957f595b0
CarvalhoNathan <fariasnathan75@gmail.com>
2025-03-19 23:26:16 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+string palavraSecreta;
+string letra;
+
+Console.WriteLine("--- Forca ---\n");
+
+Console.Write("Qual a palavra secreta? ");
+palavraSecreta = Console.ReadLine()!;
+
+Console.Write("Qual a primeira letra? ");
+letra = Console.ReadLine()!
+    .Trim()
+    .Substring(0, 1);
+
+string letraExiste = palavraSecreta.Contains(letra) ? "Sim" : "Não";
+
+Console.WriteLine($"\nA letra \"{letra}\" está na palavra secreta? {letraExiste}");
