f44d906e9c2f71f3b83db50c6c3c1d9d6518987e
Nathan Carvalho <67009187+CarvalhoNathan@users.noreply.github.com>
2025-03-24 09:52:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@ string letra;
 Console.WriteLine("--- Forca ---\n");
 
 Console.Write("Qual a palavra secreta? ");
-palavraSecreta = Console.ReadLine()!;
+palavraSecreta = Console.ReadLine()!
+    .ToLower();
 
 Console.Write("Qual a primeira letra? ");
 letra = Console.ReadLine()!
+    .ToLower()
     .Trim()
     .Substring(0, 1);
 
